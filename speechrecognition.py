626c5c11810985565bf7c3c3c281d99b343f93cf
vishvajeetsingh-bit <67941954+vishvajeetsingh-bit@users.noreply.github.com>
2020-07-08 09:46:05 +0530

    Add files via upload

---
+++ speechrecognition.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+
+AUDIO_FILE = "24.wav"
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)
+try:
+    print("audio file contains" + " " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print(" Google Speech Recognition could not understand audio")
+except sr.RequestError:
+    print("Couldn't get the results from google speech recognition")
